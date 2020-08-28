6e480a1d47f443e30fc45afa6bbf1c28f381d92e
宋元孝 <1037055457@qq.com>
2020-08-28 09:56:08 +0800

    first init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: '余温无痕个人博客',
+    description: '', //描述
+    head: [ // 注入到当前页面的 HTML <head> 中的标签
+        ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ],
+    base: '/', // 这是部署到github相关的配置
+    markdown: {
+        lineNumbers: false // 代码块显示行号
+    },
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '文档', link: '/guide/' },
+            { text: 'External', link: 'https://www.baidu.com' },
+        ],
+        sidebarDepth: 2, // 侧边栏显示2级
+        sidebar: [
+            {
+                title: '指南',
+                collapsable: false,
+                children: ['/guide/', '/guide/guide-about', '/guide/md-study', '/guide/vue-press-study'],
+            },
+            {
+                title: '文章',
+                collapsable: false,
+                children: ['/articles/vue-about', '/articles/xcx-about', '/articles/css-about','/articles/js-about'],
+            },
+        ]
+    }
+}
---
+++ docs/guide/guide-about.md
@@ -0,0 +1,4 @@
+# 关于博主
+CSDN：<https://blog.csdn.net/u014678583?orderby=ViewCount>
+
+码云：<https://gitee.com/song_yuan_xiao>
