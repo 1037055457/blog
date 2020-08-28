efbeb352a814535c160b2f0d2b649a5dc46e5afd
宋元孝 <1037055457@qq.com>
2020-08-28 10:29:03 +0800

    删除.temp

--- .temp/app-enhancers/1.js
+++
@@ -1,2 +0,0 @@
-import "C:\\Program Files\\nodejs\\node_modules\\vuepress\\node_modules\\@vuepress\\plugin-nprogress\\enhanceAppFile.js"
-export default {}
--- .temp/app-enhancers/global-components-1.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("Badge", () => import("C:\\Program Files\\nodejs\\node_modules\\vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-
-
-export default {}
